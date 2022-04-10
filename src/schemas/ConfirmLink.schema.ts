37d2cbbdccb8225a0c8703c7b1469b8b09ee2cb9
Vitalii <0x1d46@gmail.com>
2022-04-10 22:06:14 +0200

    create ConfirmLink.schema.ts

---
+++ src/schemas/ConfirmLink.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes, Types } from 'mongoose';
+import { User, UserDocument } from './User.schema';
+
+export type ConfirmLinkDocument = ConfirmLink & Document;
+
+@Schema()
+export class ConfirmLink {
+  @Prop({ type: SchemaTypes.ObjectId, ref: User.name, required: true })
+  user!: Types.ObjectId & UserDocument;
+
+  @Prop({ unique: true, required: true })
+  link: string;
+}
+
+export const ConfirmLinkSchema = SchemaFactory.createForClass(ConfirmLink);
