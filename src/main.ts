38bc76b84a8c364c65275743e73beea92c1a3f6a
Vitalii <0x1d46@gmail.com>
2022-04-10 22:07:18 +0200

    add generic app type

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   await app.listen(3000);
 }
