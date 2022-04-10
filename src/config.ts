3f63a47afb2bf012a4626aa972deeedd2725025c
Vitalii <0x1d46@gmail.com>
2022-04-10 22:06:45 +0200

    change config to nest ConfigModule

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-
-export const MONGO = process.env.MONGO;
-export const ACCESS_SECRET = process.env.ACCESS_SECRET;
-export const REFRESH_SECRET = process.env.REFRESH_SECRET;
