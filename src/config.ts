ee4831ffe533686396a79b9a0c06d5b5b6d70625
Vitalii <0x1d46@gmail.com>
2022-04-10 14:44:39 +0200

    add refresh secret

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
 
 export const MONGO = process.env.MONGO;
-export const SECRET = process.env.SECRET;
+export const ACCESS_SECRET = process.env.ACCESS_SECRET;
+export const REFRESH_SECRET = process.env.REFRESH_SECRET;
