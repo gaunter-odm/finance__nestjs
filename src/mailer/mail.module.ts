b4cdac2491ca9078140fcc05d3672151cd3226a2
Vitalii <0x1d46@gmail.com>
2022-04-10 22:06:59 +0200

    change config to nest ConfigModule

--- src/mailer/mail.module.ts
+++ src/mailer/mail.module.ts
@@ -3,7 +3,6 @@ import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { SMTP } from '../config';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfirmLink, ConfirmLinkSchema } from '../schemas/ConfirmLink.schema';
@@ -18,7 +17,7 @@ import { TokenService } from '../auth/utils/token.util';
       },
     ]),
     MailerModule.forRoot({
-      transport: SMTP,
+      transport: process.env.SMTP,
       template: {
         dir: join(__dirname, 'templates'),
         adapter: new HandlebarsAdapter(),
