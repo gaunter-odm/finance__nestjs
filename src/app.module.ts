6477486f77818cc0297dfa081e380dffd8088456
Vitalii <0x1d46@gmail.com>
2022-04-10 22:07:54 +0200

    add nodemailer service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PurchasesModule } from './purchases/purchases.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO } from './config';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mailer/mail.module';
+import { ConfigModule } from '@nestjs/config';
+import { TokenService } from './auth/utils/token.util';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
-  imports: [PurchasesModule, MongooseModule.forRoot(MONGO), AuthModule],
+  providers: [AppService, TokenService],
+  imports: [
+    PurchasesModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO),
+    AuthModule,
+    MailModule,
+  ],
 })
 export class AppModule {}
