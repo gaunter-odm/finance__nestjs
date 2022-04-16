eaf26cb92f173fab75cc6eae834ebd1bbbc7ed3c
Vitalii <0x1d46@gmail.com>
2022-04-16 02:41:01 +0200

    add expires

--- src/schemas/ConfirmLink.schema.ts
+++ src/schemas/ConfirmLink.schema.ts
@@ -1,13 +1,13 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, SchemaTypes, Types } from 'mongoose';
-import { User, UserDocument } from './User.schema';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, SchemaTypes, Types } from "mongoose";
+import { User, UserDocument } from "./User.schema";
 
 export type ConfirmLinkDocument = ConfirmLink & Document;
 
-@Schema()
+@Schema({ timestamps: true, expires: "5m" })
 export class ConfirmLink {
   @Prop({ type: SchemaTypes.ObjectId, ref: User.name, required: true })
-  user!: Types.ObjectId & UserDocument;
+  user: Types.ObjectId & UserDocument;
 
   @Prop({ unique: true, required: true })
   link: string;
